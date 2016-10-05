c28f4e14173f2491855a559fc3b0a57636b82e20
bartimar6@gmail.com <bartimar6@gmail.com>
2016-10-05 08:29:12 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Backup container for postgres instances
+
+This image provides a cron daemon that runs daily backups from postgres (clustered or single instance) to Amazon S3.
+
+Following ENV variables must be specified:
+ - `POSTGRES_HOST` contains the remote host (hostname or IP) connection string for pg_dump command line client option -h
+ - `POSTGRES_PORT` contains the remote port number for pg_dump option -P
+  - `postgresserver.domain.com:5432` in case of a single instance
+ - `POSTGRES_USER` username used for connecting to the database
+ - `POSTGRES_PASSWORD` password of user `POSTGRES_USER` who has access to all dbs
+ - `S3_URL` contains address in S3 where to store backups
+  - `bucket-name/directory`
+ - `S3_ACCESS_KEY`
+ - `S3_SECRET_KEY`
+ - `CRON_SCHEDULE` cron schedule string, default '0 2 * * *'
+
---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eo pipefail
+
+# verify variables
+if [ -z "$S3_ACCESS_KEY" -o -z "$S3_SECRET_KEY" -o -z "$S3_URL" -o -z "$POSTGRES_HOST" -o -z "$POSTGRES_PORT" ]; then
+	echo >&2 'Backup information is not complete. You need to specify S3_ACCESS_KEY, S3_SECRET_KEY, S3_URL, MYSQL_URL, MYSQL_PORT. No backups, no fun.'
+	exit 1
+fi
+
+# set s3 config
+sed -i "s/%%S3_ACCESS_KEY%%/$S3_ACCESS_KEY/g" /root/.s3cfg
+sed -i "s/%%S3_SECRET_KEY%%/$S3_SECRET_KEY/g" /root/.s3cfg
+
+# verify S3 config
+s3cmd ls "s3://$S3_URL" > /dev/null
+
+# set cron schedule TODO: check if the string is valid (five or six values separated by white space)
+[[ -z "$CRON_SCHEDULE" ]] && CRON_SCHEDULE='0 2 * * *' && \
+   echo "CRON_SCHEDULE set to default ('$CRON_SCHEDULE')"
+
+DB_URI="postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOST:$POSTGRES_PORT"
+# add a cron job
+echo "$CRON_SCHEDULE root rm -rf /tmp/dump* && pg_dumpall --dbname=$DB_URI --single-transaction --file=/tmp/dump.sql --verbose >> /var/log/cron.log 2>&1 && gzip -c /tmp/dump.sql > /tmp/dump && s3cmd sync /tmp/dump s3://$S3_URL/ >> /var/log/cron.log 2>&1 && rm -rf /tmp/dump*" >> /etc/crontab
+crontab /etc/crontab
+
+exec "$@"
