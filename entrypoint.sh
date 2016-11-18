2476138c644c618211fc98129fe811c0c6e23499
Marek Bartik <bartimar6@gmail.com>
2016-11-18 12:35:18 +0100

    POSTGRES_DB ENV var added

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 set -eo pipefail
 
 # verify variables
-if [ -z "$S3_ACCESS_KEY" -o -z "$S3_SECRET_KEY" -o -z "$S3_URL" -o -z "$POSTGRES_HOST" -o -z "$POSTGRES_PORT" ]; then
-	echo >&2 'Backup information is not complete. You need to specify S3_ACCESS_KEY, S3_SECRET_KEY, S3_URL, MYSQL_URL, MYSQL_PORT. No backups, no fun.'
+if [ -z "$S3_ACCESS_KEY" -o -z "$S3_SECRET_KEY" -o -z "$S3_URL" -o -z "$POSTGRES_HOST" -o -z "$POSTGRES_PORT" -o -z "$POSTGRES_DB" ]; then
+	echo >&2 'Backup information is not complete. You need to specify S3_ACCESS_KEY, S3_SECRET_KEY, S3_URL, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DB. No backups, no fun.'
 	exit 1
 fi
 
@@ -23,7 +23,7 @@ s3cmd ls "s3://$S3_URL" > /dev/null
 echo "*:*:*:*:$POSTGRES_PASSWORD" > ~/.pgpass
 chmod 0600 ~/.pgpass
 
-DB_URI="postgresql://$POSTGRES_USER@$POSTGRES_HOST:$POSTGRES_PORT"
+DB_URI="postgresql://$POSTGRES_USER@$POSTGRES_HOST:$POSTGRES_PORT/$POSTGRES_DB"
 # add a cron job
 echo "$CRON_SCHEDULE root rm -rf /tmp/dump* && pg_dumpall --dbname=$DB_URI --file=/tmp/dump.sql --verbose >> /var/log/cron.log 2>&1 && gzip -c /tmp/dump.sql > /tmp/dump && s3cmd sync /tmp/dump s3://$S3_URL/ >> /var/log/cron.log 2>&1 && rm -rf /tmp/dump*" >> /etc/crontab
 crontab /etc/crontab
